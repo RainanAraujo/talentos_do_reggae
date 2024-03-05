eaba8936ced58cd2e4e22e13aa03bcce3a573aef
RainanAraujo <rainanaraaujo@gmail.com>
2024-03-05 13:04:12 -0300

    feat: add home

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,6 @@
 import type { Config } from "tailwindcss";
+const colors = require("tailwindcss/colors");
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 const config: Config = {
   content: [
@@ -8,10 +10,36 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        orange: "#F2A543",
+        green: "#40AA4B",
+        red: "#C52C2B",
+        yellow: "#FCFC08",
+      },
+      animation: {
+        pulse: "pulse 3s ease-in 0s infinite normal",
+        fadeIn: "fadeIn 1s ease-in 0s normal",
+        slideToLeft: "slideToLeft 0.3s ease-in 0s normal",
+        slideToRight: "slideToRight 0.3s ease-in 0s normal",
+      },
+      keyframes: {
+        pulse: {
+          "0%": { transform: "scale(1)" },
+          "50%": { transform: "scale(1.05)" },
+          "100%": { transform: "scale(1)" },
+        },
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+        slideToLeft: {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(0)" },
+        },
+        slideToRight: {
+          "0%": { transform: "translateX(-100%)" },
+          "100%": { transform: "translateX(0)" },
+        },
       },
     },
   },

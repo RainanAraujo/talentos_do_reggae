d91fbd8b05d2825a7755384ceda8c8b681e58934
RainanAraujo <rainanaraaujo@gmail.com>
2024-03-16 11:27:44 -0300

    fix: auto update posts

--- src/app/components/InstagramViewer.tsx
+++ src/app/components/InstagramViewer.tsx
@@ -1,12 +1,12 @@
-import Image from "next/image";
 import HorizontalAnimWrapper from "./HorizontalAnimWrapper";
+import { Cards } from "@phosphor-icons/react/dist/ssr/Cards";
 
 export default async function InstagramViewer() {
   const { data } = await getData();
   return (
     <div className="gap-4 w-full flex flex-col">
       <HorizontalAnimWrapper
-        className="w-full overflow-x-auto flex no-scrollbar max-md:px-5 px-20"
+        className="w-full overflow-x-auto overflow-y-hidden flex no-scrollbar max-md:px-5 px-20"
         direction={200}
       >
         <ImagesViewer
@@ -19,7 +19,7 @@ export default async function InstagramViewer() {
       </HorizontalAnimWrapper>
       <HorizontalAnimWrapper
         direction={200}
-        className="w-full overflow-x-auto flex no-scrollbar max-md:px-5 px-20"
+        className="w-full overflow-x-auto overflow-y-hidden flex no-scrollbar max-md:px-5 px-20"
       >
         <VideoViewer
           videos={data.filter((item: any) => item.media_type === "VIDEO")}
@@ -40,7 +40,10 @@ function ImagesViewer({ images }: { images: object[] }) {
   return (
     <div className="flex gap-4">
       {images.map((image: any, index: number) => (
-        <div key={index} className="w-80 h-80 ">
+        <div
+          key={index}
+          className="w-80 h-80 relative cursor-pointer hover:scale-105 hover:transform duration-300 ease-in-out"
+        >
           <img
             width={200}
             height={200}
@@ -48,6 +51,13 @@ function ImagesViewer({ images }: { images: object[] }) {
             alt=""
             className="w-full h-full object-cover"
           />
+
+          {image.media_type === "CAROUSEL_ALBUM" && (
+            <Cards
+              className="w-8 h-8 absolute right-2 top-2 text-white "
+              weight="fill"
+            />
+          )}
         </div>
       ))}
     </div>
@@ -58,7 +68,10 @@ function VideoViewer({ videos }: { videos: object[] }) {
   return (
     <div className="flex gap-4">
       {videos.map((video: any, index: any) => (
-        <div key={index} className="w-80 h-[580px]">
+        <div
+          key={index}
+          className="w-80 h-[580px] hover:scale-105 cursor-pointer hover:transform duration-300 ease-in-out"
+        >
           <video
             src={video.media_url + "#t=0.001"}
             loop

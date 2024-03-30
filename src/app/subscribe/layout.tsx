2c56df4fab9872769c444dd9c533128505163bc1
RainanAraujo <rainanaraaujo@gmail.com>
2024-03-30 11:29:28 -0300

    fix: loading subscribe button

--- src/app/subscribe/layout.tsx
+++ src/app/subscribe/layout.tsx
@@ -10,7 +10,7 @@ export default function layoutSubscribe({
     <div className="flex flex-col items-center justify-center min-h-screen  w-full m-auto  px-20  max-md:p-0 max-w-7xl max-md:max-w-full">
       <div className="bg-neutral-900 rounded-lg h-[80vh] max-md:min-h-screen w-full flex ">
         <div className="flex flex-col justify-between flex-1 ">
-          <div className="px-20 overflow-y-scroll py-20  h-full max-md:p-5  max-md:pt-20  gap-y-5 flex flex-col relative">
+          <div className="px-20 overflow-y-auto py-20  h-full max-md:p-5  max-md:pt-20  gap-y-5 flex flex-col relative">
             <div className="space-y-2">
               <Image alt="" src={logo} width={100} height={100} />
               <h3 className="text-2xl font-bold text-white">

610617cfb43d8caec0bdf5cbec12275a4a90163d
Gildásio Chagas <gildasiochagas@Mac-Pro-de-Gildasio.local>
2024-04-30 16:29:34 -0300

    feat(Sorteador): add options and view to sorteador

--- src/app/sorteador/page.tsx
+++ src/app/sorteador/page.tsx
@@ -1,75 +1,208 @@
 "use client";
-import { useCallback, useMemo, useState } from "react";
+import clsx from "clsx";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import logo from "@/../public/logo.svg";
+import Image from "next/image";
+import Button from "../components/Button";
+import { Flipper, Flipped } from "react-flip-toolkit";
+import { shuffle } from "lodash";
 
-// Math.counter = 0;
+const djListConfig = {
+  maxSelectedItems: 7,
+  items: [
+    "ALYSSON NEY MARTINS COSTA",
+    "ANDRE HUDISON PEREIRA DA SILVA",
+    "COSMO RODRIGUES DA ANUNCIAÇÃO",
+    "DANIEL CARLOS OLIVEIRA",
+    "DENILSON CARDOSO",
+    "DIEGO RASTAMAN",
+    "DJ HELENA MELLO",
+    "DJ LION",
+    "DJ LULU ZION",
+    "DJ SOL / SOLANGE GOMES FEITOZA RODRIGUES",
+    "DJ ZE DOMINGOS ROOTS",
+    "EDILSON BLACK",
+    "ELIZABETH LAGO",
+    "ERINALDO DE JESUS SILVA GOMES",
+    "FÁBIO AIRES FALCÃO ROOTS",
+    "FRANCIVALDO SILVA FONSECA JÚNIOR",
+    "GERSON ANDREY SANTOS CHAGAS",
+    "GRACI ROOTS",
+    "IVAN SODRÉ",
+    "JANAINA SOUSA SILVA",
+    "JOENE CRISTINA COSTA LIMA",
+    "JORGE ERALDO LOUZEIRO MORAIS",
+    "JOSÉ DE ARIMATÉIA GOMES SILVA",
+    "JOSÉ RIBAMAR DA SILVA DIAS FILHO",
+    "JOSE RIBAMAR DE ASSIS MARREIROS JUNIOR",
+    "JOSÉ ROBERTO PEREIRA (DJ BETO ROOTS)",
+    "JUNIOR RAÇA",
+    "LEONARDO NOGUEIRA TAVARES",
+    "LUIS CARLOS MACEDO MELO",
+    "LUIS CRISTIANO DE AGUIAR VALE",
+    "LUIS FERNANDO CUNHA RAMOS FILHO (DJ FERNANDO ROOTS)",
+    "MARCELLO LUIZ SOUZA",
+    "MARCELO HENRIQUE ALVES COSTA",
+    "MARCELO PEDRA",
+    "MARIA DOMINGAS FERREIRA",
+    "MARILENE DE CARVALHO MENDES",
+    "MIKAEL FERREIRA SANTOS",
+    "MILTON OLIVEIRA DO LAGO JUNIOR",
+    "MOUZANIEL TEIXEIRA FERREIRA",
+    "PATRICIA FONSECA SILVA VIDAL",
+    "RAIMUNDO NONATO NEVES DOS SANTOS",
+    "RICARDO FERNANDO COSTA DE CARVALHO",
+    "ROZENILDE FERREIRA",
+    "WESLLEN SILVA ROCHA",
+  ],
+};
 
-// const frozenResults = {
-//   7: (1 / 9) * 4,
-// };
-
-// Math.randomOriginal = Math.random;
-
-// Math.random = () => {
-//   if (Math.counter in frozenResults) {
-//     Math.counter++;
-//     return frozenResults[Math.counter - 1];
-//   }
-//   Math.counter++;
-//   return Math.randomOriginal();
-// };
+const dancersListConfig = {
+  maxSelectedItems: 10,
+  items: [
+    "CARLOS MAGNO SOUSA CARDOSO E AMANDA ROCHA CANTANHEDE",
+    "DULCILENE DOS SANTOS MENDES SILVA E CARLOS HENRIQUE ALVES COSTA",
+    "FABIA DE LOURDES FRANÇA TRINDADE E JOSÉ RAIMUNDI ARAGÃO SANCHES FILHO",
+    "FELIPE PINTO DE FARIAS E DJAMILLA DANDARA SANTOS MACEDO",
+    "JAY LOBATO E WESLEY FREIRE",
+    "JOYCEANE JESUS DA SILVA E DIEGO",
+    "KEILA CRISTINA COELHO NOGUEIRA E WEVERSON JULIO NOGUEIRA MARTINS",
+    "LETÍCIA KETLEN MUNIZ LIMA E JOSÉ ORLANDO DE JESUS JÚNIOR",
+    "NILDA COELHO E ANTÔNIO JOSÉ MENDES",
+    "NÚRIA FERNANDA CUNHA GARCIA FONSECA E RAFAEL SILVA MORAES",
+    "PAULO ROGGER MUNIZ SILVA E VANDA LUCIA LEMOS",
+    "ROSÂNGELA SANTOS DE FARIAS E ALEXANDRE MAGNO OLIVEIRA MUNIZ",
+    "ROSEANA GUSMÃO BORGES E JOSUÉ LICAR DA SILVA",
+    "TERESA CRISTINA PINTO DINIZ E LUIS ANDRÉ DINIZ LOPES",
+  ],
+};
 
 export default function Drawer() {
-  const [selectedList, setSelectedList] = useState<string[]>([]);
-  const [list, setList] = useState([
-    "Item 1",
-    "Item 2",
-    "Item 3",
-    "Item 4",
-    "Item 5",
-    "Item 6",
-    "Item 7",
-    "Item 8",
-    "Item 9",
-    "Item 10",
-  ]);
+  const [categoriaSorteio, setCategoriaSorteio] = useState<
+    "DJs" | "Dancarinos"
+  >("DJs");
+  const [items, setItems] = useState(djListConfig.items);
+  const [maxSelectedItems, setMaxSelectedItems] = useState(
+    dancersListConfig.maxSelectedItems
+  );
+  const [current, setCurrent] = useState(0);
+  const [results, setResults] = useState({ DJs: null, Dancarinos: null });
 
-  const availableList = useMemo(
-    () => list.filter((item) => !selectedList.includes(item)),
-    [list, selectedList]
+  const shuffleList = useCallback(
+    (currentIndex: number) => {
+      const availableList = items.slice(currentIndex);
+      const shuffledList = shuffle(availableList);
+      const newList = [...items.slice(0, currentIndex), ...shuffledList];
+      setItems(newList);
+      setCurrent(currentIndex + 1);
+      if (currentIndex === items.length) {
+        setResults({ ...results, [categoriaSorteio]: newList });
+      }
+    },
+    [items, results, categoriaSorteio]
   );
 
-  const handleSelect = useCallback((item: string) => {
-    setSelectedList((prev) => [...prev, item]);
-  }, []);
+  useEffect(() => {
+    if (current < items.length && current > 0) {
+      setTimeout(() => {
+        shuffleList(current);
+      }, 500);
+    }
+  }, [items]);
 
-  // window["randomRange"] = (start: number, end: number) =>
-  //   Math.floor(Math.random() * (end - start)) + start;
+  useEffect(() => {
+    if (categoriaSorteio === "DJs") {
+      setItems(results.DJs ?? djListConfig.items);
+      setMaxSelectedItems(djListConfig.maxSelectedItems);
+      setCurrent(results.DJs ? djListConfig.items.length : 0);
+    }
+    if (categoriaSorteio === "Dancarinos") {
+      setItems(results.Dancarinos ?? dancersListConfig.items);
+      setMaxSelectedItems(dancersListConfig.maxSelectedItems);
+      setCurrent(results.Dancarinos ? dancersListConfig.items.length : 0);
+    }
+  }, [categoriaSorteio]);
 
   return (
     <>
-      <div className="flex flex-col items-center justify-center min-h-screen  w-full m-auto  px-20  max-md:p-0 max-w-7xl max-md:max-w-full">
-        <div className="bg-neutral-900 rounded-lg h-[80vh] max-md:min-h-screen w-full flex ">
-          <div className="flex flex-col justify-between flex-1 ">
-            <div className="px-20 overflow-hidden py-20  h-full max-md:p-5  max-md:pt-20  gap-y-5 flex flex-col relative ">
-              <p>Items: {availableList.join(", ")}</p>
-              <p>Selected: {selectedList.join(", ")}</p>
-              <button
-                onClick={() => {
-                  do {
-                    var selectedIndex = Math.floor(
-                      Math.random() * availableList.length
-                    );
-                  } while (
-                    !(selectedIndex in availableList) &&
-                    availableList.length != 0
-                  );
-                  if (selectedIndex in availableList)
-                    handleSelect(availableList[selectedIndex]);
-                }}
-              >
-                teste
-              </button>
+      <div className="flex flex-col items-center justify-center min-h-screen  w-full m-auto  bg-neutral-900  ">
+        <div className="flex flex-col justify-between flex-1 w-full items-center ">
+          <div className="px-20 overflow-hidden py-20 w-full h-full  gap-y-3 flex flex-col relative ">
+            <div className="flex gap-6 w-full items-center justify-between mb-4">
+              <Image
+                src={logo}
+                alt="Talentos do Reggae"
+                width={200}
+                height={200}
+                className={clsx("animate-pulse w-44", "max-md:w-[50%]")}
+              />
+              <div className="gap-2 flex flex-col">
+                <h1 className="text-5xl font-bold">SORTEIO</h1>
+                <div className="h-20">
+                  <Button
+                    className={clsx(
+                      categoriaSorteio == "DJs" && "bg-green text-xl"
+                    )}
+                    onClick={() => setCategoriaSorteio("DJs")}
+                  >
+                    DJs
+                  </Button>
+                  <Button
+                    className={clsx(
+                      categoriaSorteio == "Dancarinos" && "bg-green text-xl"
+                    )}
+                    onClick={() => setCategoriaSorteio("Dancarinos")}
+                  >
+                    Dançarinos
+                  </Button>
+                </div>
+              </div>
+              <div className="flex items-center gap-4">
+                <div className="flex flex-col  gap-2 items-center">
+                  <div
+                    className="w-40 h-40 bg-red rounded-full cursor-pointer"
+                    onClick={() => {
+                      shuffleList(0);
+                    }}
+                  />
+                  <h1 className="text-3xl *:font-bold">Sortear</h1>
+                </div>
+              </div>
             </div>
+            <hr className="border-zinc-700" />
+
+            <Flipper
+              staggerConfig={{
+                default: {
+                  speed: 0.1,
+                },
+              }}
+              flipKey={items.join("")}
+            >
+              <div className="flex flex-col gap-1">
+                {items.map((v, i) => {
+                  let status = "";
+                  if (i < current) {
+                    if (i < maxSelectedItems) {
+                      status = `${i + 1}º Selecionado`;
+                    } else {
+                      status = `${i - maxSelectedItems + 1}º Suplente`;
+                    }
+                  }
+                  return (
+                    <Flipped key={v} flipId={v}>
+                      <div>
+                        <div className="w-full p-4 ga-2 text-xl flex justify-between items-center">
+                          <div>{v}</div>
+                          <div>{status}</div>
+                        </div>
+                        <hr className="border-zinc-700" />
+                      </div>
+                    </Flipped>
+                  );
+                })}
+              </div>
+            </Flipper>
           </div>
         </div>
       </div>

e97436a1bcc46c967556bd4f556fbd9f650d0353
RainanAraujo <rainanaraaujo@gmail.com>
2024-07-05 18:47:38 -0300

    adjust info

---
+++ src/configs/documents.ts
@@ -0,0 +1,51 @@
+export const DOCUMENTOS = [
+  {
+    nome: "ALTERAÇÃO DO CRONOGRAMA",
+    descricao: "Acesse a divulgação da alteração do cronograma.",
+    url: "/alteracao_do_cronograma.pdf",
+  },
+  {
+    nome: "Divulgação final dos DJs e dançarinos selecionados por sorteio",
+    descricao:
+      "Acesse a divulgação final dos DJs e dançarinos selecionados por sorteio",
+    url: "/final_sorteio_djs_dancarinos.pdf",
+  },
+  {
+    nome: "Divulgação final das bandas e cantores selecionados",
+    descricao: "Acesse a divulgação final das bandas e cantores selecionados",
+    url: "/final_bandas_cantores.pdf",
+  },
+  {
+    nome: "Divulgação final dos DJs e dançarinos classificados para o sorteio",
+    descricao:
+      "Acesse a divulgação final dos DJs e dançarinos classificados para o sorteio",
+    url: "/final_djs_dancarinos.pdf",
+  },
+  {
+    nome: "Divulgação preliminar das bandas e cantores selecionados",
+    descricao:
+      "Acesse a divulgação preliminar das bandas e cantores selecionados",
+    url: "/preliminar_bandas_cantores.pdf",
+  },
+  {
+    nome: "Divulgação preliminar dos DJs e dançarinos classificados para o sorteio",
+    descricao:
+      "Acesse a divulgação preliminar dos DJs e dançarinos classificados para o sorteio",
+    url: "/preliminar_djs_dancarinos.pdf",
+  },
+  {
+    nome: "Retificação 02 do Edital de Abertura",
+    descricao: "Acesse a retificação 02 do Edital de Abertura",
+    url: "/retificacao2.pdf",
+  },
+  {
+    nome: "Retificação 01 do Edital de Abertura",
+    descricao: "Acesse a retificação 01 do Edital de Abertura",
+    url: "/retificacao.pdf",
+  },
+  {
+    nome: "Edital de abertura",
+    descricao: "Acesse o edital do evento",
+    url: "/edital.pdf",
+  },
+];
